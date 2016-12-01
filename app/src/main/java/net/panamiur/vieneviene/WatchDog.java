1ae09b292796a5ee31f1e2849a4a1d07814fa6ca
Leonardo Pereyra <leonardo.pereyra@go-sharp.net>
2016-12-01 15:38:53 -0600

    Geofence

--- app/src/main/java/net/panamiur/vieneviene/WatchDog.java
+++ app/src/main/java/net/panamiur/vieneviene/WatchDog.java
@@ -11,6 +11,7 @@ import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -27,22 +28,28 @@ import net.panamiur.vieneviene.services.GeofenceTransitionsIntentService;
 
 import java.util.ArrayList;
 
-import static android.R.attr.id;
-
 public class WatchDog extends Activity implements
         GoogleApiClient.ConnectionCallbacks,
         GoogleApiClient.OnConnectionFailedListener,
         ResultCallback<Status>,
-        LocationListener{
+        LocationListener
+
+{
+
+
     protected ArrayList<Geofence> geofenceList;
-    protected GoogleApiClient googleApiClient;
+
     private Button btnddGeofence;
     public static final long GEOFENCE_EXPIRATION_IN_MILLISECONDS = 12 * 60 * 60 * 1000;
-    public static final float GEOFENCE_RADIUS_IN_METERS = (float) .5;
+    public static final float GEOFENCE_RADIUS_IN_METERS = (float) 3;
+    private static final String GEOFENCE_REQ_ID = "My Geofence";
     private LocationManager locationManager;
-    Location location;
+
+
+    private Location location;
     String provider;
     double lat, lon;
+    protected GoogleApiClient googleApiClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,34 +59,58 @@ public class WatchDog extends Activity implements
         geofenceList = new ArrayList<Geofence>();
         locationManager =(LocationManager)getSystemService(Context.LOCATION_SERVICE);
 
-        Criteria criteria = new Criteria();
-         provider= locationManager.getBestProvider(criteria, false);
-        if(provider != null & !provider.equals("")){
-             location = locationManager.getLastKnownLocation(provider);
+        initAcurracy();
+        onLocationChanged(location);
 
-            locationManager.requestLocationUpdates(provider,5000,10,this);
-
-            if(location!=null){
-                onLocationChanged(location);
-            }else{
-                Toast.makeText(this, "Not Found", Toast.LENGTH_SHORT).show();
-            }
-        }else {
-            Toast.makeText(this, "Provider is null", Toast.LENGTH_SHORT).show();
-        }
         btnddGeofence.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+            addGeofences();
 
             }
         });
-
-        addGeogences();
-        buildGoogleApiClient();
+        createGoogleApi();
 
     }
 
+    private void initAcurracy() {
 
+        Criteria criteria = new Criteria();
+        provider = locationManager.getBestProvider(criteria, false);
+        if (provider != null & !provider.equals("")) {
+            location = locationManager.getLastKnownLocation(provider);
+
+            if (location != null) {
+                onLocationChanged(location);
+            } else {
+                Toast.makeText(this, "Not Found", Toast.LENGTH_SHORT).show();
+            }
+        } else {
+            Toast.makeText(this, "Provider is null", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void createGoogleApi() {
+        if (googleApiClient == null) {
+            googleApiClient = new GoogleApiClient.Builder(this)
+                    .addConnectionCallbacks(this)
+                    .addOnConnectionFailedListener(this)
+                    .addApi(LocationServices.API)
+                    .build();
+        }
+    }
+
+    public Geofence geofence() {
+        String id="geo";
+
+        Geofence geofence = new Geofence.Builder()
+                .setRequestId(id)
+                .setTransitionTypes(Geofence.GEOFENCE_TRANSITION_EXIT)
+                .setCircularRegion(lat, lon, GEOFENCE_RADIUS_IN_METERS)
+                .setExpirationDuration(Geofence.NEVER_EXPIRE)
+                .build();
+        return geofence;
+    }
 
 
     @Override
@@ -92,11 +123,8 @@ public class WatchDog extends Activity implements
 
         googleApiClient.connect();
     }
-
             @Override
             public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
-
-
             }
 
             @Override
@@ -109,8 +137,8 @@ public class WatchDog extends Activity implements
                     ).show();
                 } else {
                     // Get the status code for the error and log it using a user-friendly message.
-                    String errorMessage = "Error" ;               }
-
+                            status.getStatusCode();
+                }
             }
 
 
@@ -118,6 +146,7 @@ public class WatchDog extends Activity implements
     public void onLocationChanged(Location location) {
         lat=location.getLatitude();
         lon=location.getLongitude();
+        Toast.makeText(this, "lat" + lat + "lon" + lon, Toast.LENGTH_SHORT).show();
     }
 
     @Override
@@ -135,27 +164,6 @@ public class WatchDog extends Activity implements
 
     }
 
-    public void addGeogences(){
-        onLocationChanged(location);
-        geofenceList.add (new Geofence.Builder()
-                .setRequestId(Long.toString(id))
-                .setCircularRegion(19.432337,-99.192584, GEOFENCE_RADIUS_IN_METERS)
-                .setExpirationDuration(GEOFENCE_EXPIRATION_IN_MILLISECONDS)
-                .setTransitionTypes(Geofence.GEOFENCE_TRANSITION_ENTER | Geofence.GEOFENCE_TRANSITION_EXIT)
-        .build())
-        ;
-
-    }
-
-    protected synchronized void buildGoogleApiClient() {
-        googleApiClient = new GoogleApiClient.Builder(this)
-                .addConnectionCallbacks(this)
-                .addOnConnectionFailedListener(this)
-                .addApi(LocationServices.API)
-                .build();
-    }
-
-
     @Override
     protected void onStart() {
         super.onStart();
@@ -172,9 +180,9 @@ public class WatchDog extends Activity implements
         }
     }
 
-    public void addGeofencesButtonHandler(View view) {
+    public void addGeofences(){
         if(!googleApiClient.isConnected()){
-            Toast.makeText(this, "Google API Client not connected!", Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, "Google API CLIENT not connected", Toast.LENGTH_SHORT).show();
             return;
         }
 
@@ -185,21 +193,22 @@ public class WatchDog extends Activity implements
                     getGeofencePendingIntent()
             ).setResultCallback(this);
         }catch (SecurityException e){
-
+            Log.e("error", "error: "+ e);
         }
     }
 
     private GeofencingRequest getGeofencingRequest(){
         GeofencingRequest.Builder builder = new GeofencingRequest.Builder();
-        builder.setInitialTrigger(GeofencingRequest.INITIAL_TRIGGER_ENTER);
-        builder.addGeofences(geofenceList);
+        builder.setInitialTrigger(GeofencingRequest.INITIAL_TRIGGER_EXIT);
+        builder.addGeofence(geofence());
         return builder.build();
     }
 
-    private PendingIntent getGeofencePendingIntent() {
+    private PendingIntent getGeofencePendingIntent(){
         Intent intent = new Intent(this, GeofenceTransitionsIntentService.class);
-        // We use FLAG_UPDATE_CURRENT so that we get the same pending intent back when calling addgeoFences()
-        return PendingIntent.getService(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+        return PendingIntent.getService(this,0,intent,PendingIntent.FLAG_UPDATE_CURRENT);
     }
+
+
 
 }
--- app/src/main/java/net/panamiur/vieneviene/services/GeofenceTransitionsIntentService.java
+++ app/src/main/java/net/panamiur/vieneviene/services/GeofenceTransitionsIntentService.java
@@ -1,13 +1,14 @@
 package net.panamiur.vieneviene.services;
 
 import android.app.IntentService;
+import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
+import android.app.TaskStackBuilder;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
-import android.support.v4.app.TaskStackBuilder;
-import android.support.v7.app.NotificationCompat;
+import android.support.v4.app.NotificationCompat;
 import android.text.TextUtils;
 import android.util.Log;
 
@@ -15,6 +16,7 @@ import com.google.android.gms.location.Geofence;
 import com.google.android.gms.location.GeofenceStatusCodes;
 import com.google.android.gms.location.GeofencingEvent;
 
+import net.panamiur.vieneviene.R;
 import net.panamiur.vieneviene.WatchDog;
 
 import java.util.ArrayList;
@@ -28,57 +30,49 @@ public class GeofenceTransitionsIntentService extends IntentService {
 
     protected static final String TAG = "GeofenceTransitionsIS";
 
-    public GeofenceTransitionsIntentService() {
-        super(TAG);  // use TAG to name the IntentService worker thread
-    }
 
+    public GeofenceTransitionsIntentService() {
+        super(TAG);
+    }
 
     @Override
     protected void onHandleIntent(Intent intent) {
         GeofencingEvent event = GeofencingEvent.fromIntent(intent);
-        if(event.hasError()){
-            Log.e(TAG, "GeofencingEvent Error: " + event.getErrorCode());
+        if (event.hasError()) {
+            Log.e(TAG, "Errot: " + event.getErrorCode());
             return;
         }
         String description = getGeofenceTransitionDetails(event);
         sendNotification(description);
     }
+
     private static String getGeofenceTransitionDetails(GeofencingEvent event) {
-        String transitionString =
-                GeofenceStatusCodes.getStatusCodeString(event.getGeofenceTransition());
-        List triggeringIDs = new ArrayList();
+        String transition = GeofenceStatusCodes.getStatusCodeString(event.getGeofenceTransition());
+
+        List triggerinIDs = new ArrayList();
         for (Geofence geofence : event.getTriggeringGeofences()) {
-            triggeringIDs.add(geofence.getRequestId());
+            triggerinIDs.add(geofence.getRequestId());
         }
-        return String.format("%s: %s", transitionString, TextUtils.join(", ", triggeringIDs));
+        Log.e("geo", "size"+triggerinIDs.size());
+        return String.format("%s: %s", transition, TextUtils.join(", ", triggerinIDs));
     }
 
     private void sendNotification(String notificationDetails) {
-        // Create an explicit content Intent that starts MainActivity.
         Intent notificationIntent = new Intent(getApplicationContext(), WatchDog.class);
 
-        // Get a PendingIntent containing the entire back stack.
         TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
         stackBuilder.addParentStack(WatchDog.class).addNextIntent(notificationIntent);
-        PendingIntent notificationPendingIntent =
-                stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
+        PendingIntent notificationPendingIntent = stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
 
-        // Get a notification builder that's compatible with platform versions >= 4
         NotificationCompat.Builder builder = new NotificationCompat.Builder(this);
 
-        // Define the notification settings.
-        builder.setColor(Color.RED)
-                .setContentTitle(notificationDetails)
-                .setContentText("Click notification to return to App")
+        builder.setContentTitle(notificationDetails)
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setContentText("Salio del radio")
                 .setContentIntent(notificationPendingIntent)
                 .setAutoCancel(true);
-
-        // Fire and notify the built Notification.
-        NotificationManager notificationManager =
-                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
         notificationManager.notify(0, builder.build());
+
     }
-
-
-
 }
